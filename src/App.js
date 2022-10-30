c902305e985dda500af4eea2439902ef3fe2e31a
Rati <ratigumashvili@gmail.com>
2022-10-30 21:44:52 +0400

    Changed folder name to components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Modal from "./modal/Modal";
+import Modal from "./components/Modal";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
